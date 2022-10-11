551142472098dde44545b5d97bef3e14266c92de
marcelostockle <marcelostockle@hotmail.com>
2022-10-11 15:35:44 -0300

    ADD project setup
    - .gitignore
    - Dockerimage and compose files
    - essential React libraries and proxy configuration (package.json / setupProxy.js)
    - React boilerplate homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <span className="header-title">CUSTOM TATOEBA UTILITIES</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
