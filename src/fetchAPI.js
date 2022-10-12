54daaf6b28236e67392a45a8c1e2dc978232cb2d
marcelostockle <marcelostockle@hotmail.com>
2022-10-12 16:59:36 -0300

    UPDATE Tatoeba fetch request
    Included required header "SameSite"

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -9,6 +9,7 @@ export default function fetchAPI(setSentence, lang) {
             "accept": "application/json, text/plain, */*",
             "accept-language": "en-US,en;q=0.9,ja;q=0.8",
             "x-requested-with": "XMLHttpRequest",
+            "SameSite": "strict",
             "Referrer-Policy": "strict-origin-when-cross-origin"
         },
         mode: "cors"
