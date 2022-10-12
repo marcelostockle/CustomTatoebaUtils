2fa5d1a3a09de075319fe5d927724f7d48a04a35
marcelostockle <marcelostockle@hotmail.com>
2022-10-12 17:00:33 -0300

    UPDATE Tatoeba direct links
    Sentence link to original entry at Tatoeba.org

--- src/components/wordScramble.js
+++ src/components/wordScramble.js
@@ -2,26 +2,36 @@ import React, {useState} from 'react'
 import Stack from '@mui/material/Stack';
 import LangFlag from './langFlag';
 import ControlRow from './controlRow'
+import { Info } from '@mui/icons-material'
 import './wordScramble.css'
 
 const WordScramble = () => {
     const defSentence = { sentence: {
         text: "Hit [Next] to start",
         lang: "unknown",
+        id: 0,
         translations: [[]]
     }}
     const [sentence, setSentence] = useState(defSentence)
     // Home page outline
     return (
         <div className="wordScramble">
-            <Stack direction="column" alignItems="flex-start">
+            <Stack direction="column" alignItems="flex-start" width="inherit">
                 <ControlRow setSentence={setSentence}/>
                 <Stack direction="row">
+                    <a href={`https://tatoeba.org/en/sentences/show/${sentence.sentence.id}`}
+                        style={{display: "flex", alignItems: "center"}}>
+                        <Info color="action"/>
+                    </a>
                     <LangFlag lang={sentence.sentence.lang}/>
                     <h2 className="mainSentence">{sentence.sentence.text}</h2>
                 </Stack>
                 {sentence.sentence.translations[0].map(tSentence =>
                     <Stack key={`s${tSentence.id}`} direction="row">
+                        <a href={`https://tatoeba.org/en/sentences/show/${tSentence.id}`}
+                            style={{display: "flex", alignItems: "center"}}>
+                            <Info color="action"/>
+                        </a>
                         <LangFlag lang={tSentence.lang}/>
                         <h4 className="tSentence">{tSentence.text}</h4>
                     </Stack>
