7b961e3aafea9b042231b489d2b09e54b529ac8a
marcelostockle <marcelostockle@hotmail.com>
2022-10-18 02:27:10 -0300

    UPDATE request headers

--- netlify/functions/tatoeba-random/index.js
+++ netlify/functions/tatoeba-random/index.js
@@ -2,7 +2,13 @@ import fetch from 'node-fetch'
 
 export const handler = async (event, context) => {
   const API_ENDPOINT = `https://tatoeba.org/en/sentences/random/${event.queryStringParameters.lang}`
-  const response = await fetch(API_ENDPOINT)
+  const response = await fetch(API_ENDPOINT, {
+    method: 'get',
+    headers: {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "en-US,en;q=0.9,ja;q=0.8",
+    }
+  })
   const data = await response.json()
 
   return {
