9a7cac90f17c8094190d29db9d0d142d55e356ad
marcelostockle <marcelostockle@hotmail.com>
2022-10-18 02:10:37 -0300

    UPDATE aimless patch

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,28 +1,18 @@
 import axios from 'axios'
 import { trackPromise } from 'react-promise-tracker'
 
-const fetchAPI = async (setSentence, lang) => {
-    trackPromise(
-        await axios({
-            method: "GET",
-            url: `/.netlify/functions/tatoeba-random`,
-            params: {lang},
-            headers: {
-                "accept": "application/json, text/plain, */*",
-                "accept-language": "en-US,en;q=0.9,ja;q=0.8",
-                "x-requested-with": "XMLHttpRequest",
-                "SameSite": "strict",
-                "Referrer-Policy": "strict-origin-when-cross-origin"
-            },
-            mode: "cors"
-        }).then(res => {
-            setSentence(res.data)
-            console.log(res.data)
-        }).catch(e => {
-            if (axios.isCancel(e)) { return }
-            console.log(e)
-        })
-    )
+const fetchPromise = async (setSentence, lang) => {
+    const res = await axios({
+        method: "GET",
+        url: `/.netlify/functions/tatoeba-random`,
+        params: {lang},
+    })
+    const { data } = await res
+    setSentence(res.data)
+    console.log(res.data)
+}
+const fetchAPI = (setSentence, lang) => {
+    trackPromise(fetchPromise(setSentence, lang))
 }
 
 export default fetchAPI
